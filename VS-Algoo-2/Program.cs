167506540ece9327e567eee3be2f74342a8731c8
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-10-20 17:36:31 +0700

    memasukkan kode dan membuat console writeline

--- VS-Algoo-2/Program.cs
+++ VS-Algoo-2/Program.cs
@@ -85,6 +85,13 @@ namespace QuickSort
             Console.WriteLine("n====================");
             Console.WriteLine("Sorted array elements");
             Console.WriteLine("=====================");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
         }
     }
 }
